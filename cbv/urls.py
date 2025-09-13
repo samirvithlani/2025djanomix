494043ea36a19037abd33445b9ce2c5a2fffc4ec
samirvithlani <samir.vithlani83955@gmail.com>
2025-09-13 19:32:02 +0530

    cbv

--- cbv/urls.py
+++ cbv/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("myfun/",views.MyView.as_view(),name="myfun"),
     path("home/",views.HomePageView.as_view()),
-    path("sportslist/",views.SportsListView.as_view(),name='sportslist')
+    path("sportslist/",views.SportsListView.as_view(),name='sportslist'),
+    path("createsport/",views.SportsCreateView.as_view(),name="createsport"),
+    path("sportsdetail/<int:pk>",views.SportsDetailView.as_view(),name="sportdetail")
 ]
--- cbv/views.py
+++ cbv/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,HttpResponse
 from django.views import View
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView,ListView,DetailView
+from django.views.generic.edit import CreateView
 from . import models
+from django.urls import reverse_lazy
 # Create your views here.
 #View
 class MyView(View):
@@ -18,5 +20,53 @@ class HomePageView(TemplateView):
 class SportsListView(ListView):
     model = models.Sports
     template_name = "sports/sportsList.html"    
-    context_object_name = "sports"
+    context_object_name = "sports" 
+    #paginate_by =2
+    paginate_orphans=False
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "ALL SPORTS"
+        context['totalSports'] = self.get_queryset().count()
+        print(context)
+        return context
+    
+    def get_ordering(self):
+        order = self.request.GET.get("name","origin")
+        return order
+    
+
+class SportsCreateView(CreateView):
+    model = models.Sports
+    template_name = "sports/sports_create.html"        
+    fields = "__all__"
+    success_url = reverse_lazy("sportslist")
+    #skwarg{pk:"",slug:""}
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] ="this is sports creation page"
+        return context
+    
+    def form_valid(self,form):
+        #user loggedin --> sports create..
+        print("form...",form)
+        print("//",form.instance.origin)
+        form.instance.name = form.instance.name.upper() 
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        #return super().get_success_url()
+        return reverse_lazy("sportdetail",kwargs={'pk':self.object.pk})
+
+
+class SportsDetailView(DetailView):
+    model = models.Sports
+    context_object_name="sports"
+    template_name = "sports/sport_Detail.html"    
+    
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    
     
