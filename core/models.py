23780592274c60d0d0fb4ae1a12a100797668309
samirvithlani <samir.vithlani83955@gmail.com>
2025-09-27 18:56:15 +0530

    changes

--- core/models.py
+++ core/models.py
@@ -13,12 +13,15 @@ class CustomeUserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email = email,**extra_fields)
         user.set_password(password) #hashed password
-        user.save(using=self.db)
+        user.save(using=self._db)
         return user
     def create_superuser(self,email,password,**extra_fields):
         extra_fields.setdefault('is_staff',True)
         extra_fields.setdefault('is_superuser',True)
-        return self.create_user(email,password,extra_fields)
+        return self.create_user(email,password,**extra_fields)
+    
+    def get_by_natural_key(self, email):
+        return self.get(email__iexact=email) 
 
 
 #custom userModel
@@ -31,6 +34,8 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     USERNAME_FIELD="email"
     REQUIRED_FIELDS = ["name"]
     
+    objects = CustomeUserManager()
+
     def __str__(self):
         return self.email
     
